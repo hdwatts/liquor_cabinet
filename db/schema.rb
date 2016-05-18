c0eb8978e270a99d43d96a95173822fb1774594c
daniellacor <daniella.corricelli@gmail.com>
2016-05-18 16:10:13 -0400

    added email to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518155967) do
+ActiveRecord::Schema.define(version: 20160518200722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,8 +86,10 @@ ActiveRecord::Schema.define(version: 20160518155967) do
     t.datetime "birth_date"
     t.text     "bio"
     t.string   "img_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.string   "email"
   end
 
 end
