f46d863a15d2eb453a6f83652a7a9a692c199e6a
daniellacor <daniella.corricelli@gmail.com>
2016-05-18 17:28:43 -0400

    Added password to users strong params in sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:username, :first_name, :last_name, :birth_date, :bio, :img_url)
+    params.require(:user).permit(:username, :first_name, :last_name, :password, :birth_date, :bio, :img_url)
   end
 
 end
