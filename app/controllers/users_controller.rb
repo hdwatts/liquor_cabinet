8779684635ff11d5d964c6013a04e1794a73ce7a
Eve <eve.essex@gmail.com>
2016-05-18 16:35:57 -0400

    added user crud actions and login sessions/controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new 
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else 
+      render 'new'  
+    end
+  end
+
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:username, :birth_date, :bio, :img_url)
+  end
+
 end
