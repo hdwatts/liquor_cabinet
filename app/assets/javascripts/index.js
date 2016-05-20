31d3fa55b149436cbd3f5f43de5689019b42efc2
Eve <eve.essex@gmail.com>
2016-05-20 11:57:43 -0400

    repaired sort buttons

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,20 +3,19 @@ $(function() {
 })
 
 function get_sort_params() {
-  var data
-
+  var param_to_sort_by
   $('#index_filter .btn').on('click', function() {
     if ($( this ).hasClass('date')) {
-      data = "date"
+      param_to_sort_by = "date"
   } else if ($( this ).hasClass('difficulty')) {
-      data = "difficulty"
+      param_to_sort_by = "difficulty"
   } else if ($( this ).hasClass('servings')) {
-      data = 'servings'
+      param_to_sort_by = 'servings'
     }
     $.ajax({
     method: "GET",
     url: "/recipe/sort",
-    data: data
+    data: param_to_sort_by
     }).done(function( data ) {
     $("#index_recipes").empty();
     $("#index_recipes").append(data);
