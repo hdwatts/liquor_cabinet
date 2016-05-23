318bff4395c2afe6af4bd81286afa66d037eee01
hdwatts <howard@hdwatts.com>
2016-05-23 19:17:18 -0400

    Ajax -> home controller lazy fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @recipes = Recipe.sort_by_date(Recipe.all)
+    if params[:limit] == "" || !params[:limit]
+      params[:limit] = 15
+    end
+
+    @recipes = @recipes.slice(0, params[:limit].to_i)
+
     @max_servings = Recipe.maximum(:servings)
   end
 
