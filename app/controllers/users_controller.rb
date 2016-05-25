8de901858577cccc410d465296c5086f499df4f9
hdwatts <howard@hdwatts.com>
2016-05-25 12:04:53 -0400

    Added @

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:success] = "Welcome #{@user.first_name}!"
       redirect_to @user
     else
