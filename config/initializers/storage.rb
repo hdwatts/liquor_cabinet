bfd915825a95adf876f7404a810bd942450cacf7
hdwatts <howard@hdwatts.com>
2016-05-26 15:06:11 -0400

    AWS BULLSHIT

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -6,7 +6,6 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     ENV['aws_access_id'],
       aws_secret_access_key: ENV['aws_access_secret_key'],
-      region: 'US Standard'
   }
   config.fog_directory  = "cocktailcabinet"
   config.fog_public     = false
