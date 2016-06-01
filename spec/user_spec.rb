00c7cab0f1ce6e9b3e2a54a0792243dce953a1f7
Eve <eve.essex@gmail.com>
2016-06-01 16:03:51 -0400

    update user model spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  # let(:user) {User.create(email: "eve@yo.com", first_name: "eve", last_name: "essex", birth_date: "1990-12-12", password: "asdf")}
+  # let(:user) {User.create(email: "jane@yo.com", first_name: "jane", last_name: "doe", birth_date: "1990-12-12", password: "asdf")}
   # let(:recipe1) {Recipe.create(name: "recipe1")}
   # let(:recipe2) {Recipe.create(name: "recipe2")}
 
   it "is invalid without a real email address" do
-    user1 = User.new(email: "eve@gmail")
+    user1 = User.new(email: "jane@gmail")
     user1.valid?
     expect(user1.errors[:email]).to eq(["is not an email"])
   end
 
   it "is invalid without a unique email address" do  
-    user1 = User.create(email: "eve@yo.com", first_name: "eve", last_name: "essex", birth_date: "1990-12-12", password: "asdf")
-    user2 = User.create(email: "eve@yo.com", first_name: "eve", last_name: "essex", birth_date: "1990-12-12", password: "asdf")
+    user1 = User.create(email: "jane@yo.com", first_name: "jane", last_name: "doe", birth_date: "1990-12-12", password: "asdf")
+    user2 = User.create(email: "jane@yo.com", first_name: "jane", last_name: "doe", birth_date: "1990-12-12", password: "asdf")
     user2.valid?
     expect(user2.errors[:email]).to eq(["has already been taken"])
   end
